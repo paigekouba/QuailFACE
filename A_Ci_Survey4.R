c2c9d4a226e6e1cce689095ddc67d4a1e0a5f5b0
paigekouba <pvkouba@ucdavis.edu>
2023-11-14 22:55:54 -0800

    Added plots with A/Ci curves (3- or 4-point). Checked for # of "keepers" (curves that cross 400ppm Ci).

--- A_Ci_Survey4.R
+++ A_Ci_Survey4.R
@@ -33,3 +33,23 @@ df <- left_join(LiCOR_4, crosswalk, by = join_by(LiCOR_ID))
 # now has each LiCOR measurement associated with plant ID. sets of 3 and 4. CO2 setting goes 400-600-800(-400)
 
 test_df <- df[1:23,c(3, 6:8, 41, 86:93)]
+library(ggplot2)
+ggplot(test_df, aes(x = Ci.x, y = Photo.x, colour = Treatment)) +
+geom_point() +
+  facet_wrap( ~ ID) +
+geom_vline(xintercept = 400, colour="blue")
+
+ggplot(df, aes(x = Ci.x, y = Photo.x, colour = Treatment)) +
+  geom_point() +
+  facet_wrap( ~ ID) +
+  geom_vline(xintercept = 400, colour="blue")
+
+df %>% 
+  select(HHMMSS, Photo.x, Cond, Ci.x, CO2R, SWC, Date, Log, X., Time, ID, Plot, Treatment) %>% 
+  group_by(ID, Treatment) %>% 
+  summarize(ambient = if_else(max(Ci.x)>400, TRUE, FALSE)) %>% 
+  group_by(Treatment) %>% 
+  summarize(keepers = sum(ambient)) %>% 
+  View()
+# AD = 26   AW = 15    ED = 30   EW = 33 not correct
+# can't get it to count right; it's adding 1 for every observation, rather than every group of obs (i.e. each plant)
