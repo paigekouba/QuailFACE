d84325d798fc2f428261c00c25587e6578c1ced6
paigekouba <pvkouba@ucdavis.edu>
2023-11-16 16:23:53 -0800

    Break down results by species, treatment. Started on interpolated Photo.x values

--- A_Ci_Survey4.R
+++ A_Ci_Survey4.R
@@ -5,6 +5,7 @@
 setwd("~/Documents/UC_Davis/2021_Winter/Quals/Proposal/Chapter 1/TinyFACE/GitHub/QuailFACE")
 LiCOR_4 <- read.csv("RawData/Quail_LiCOR4_data.csv")
 crosswalk <- read.csv("RawData/Survey4.csv")
+library("stringr")  
 library(tidyverse)
 head(LiCOR_4)
 head(crosswalk)
@@ -42,7 +43,7 @@ geom_vline(xintercept = 400, colour="blue")
 ggplot(df, aes(x = Ci.x, y = Photo.x, colour = Treatment)) +
   geom_point() +
   facet_wrap( ~ ID) +
-  geom_vline(xintercept = 400, colour="blue")
+  geom_vline(xintercept = 333, colour="blue")
 
 df %>% 
   select(HHMMSS, Photo.x, Cond, Ci.x, CO2R, SWC, Date, Log, X., Time, ID, Plot, Treatment) %>% 
@@ -51,5 +52,57 @@ df %>%
   group_by(Treatment) %>% 
   summarize(keepers = sum(ambient)) %>% 
   View()
-# AD = 26   AW = 15    ED = 30   EW = 33 not correct
-# can't get it to count right; it's adding 1 for every observation, rather than every group of obs (i.e. each plant)
+# AD = 8   AW = 5    ED = 9   EW = 10 
+
+# MG says to take the average midpoint of all curves and make that the cutoff. Also a simple two-way ANOVA of the interpolated values.
+midpoints <- df %>% 
+  select(HHMMSS, Photo.x, Cond, Ci.x, CO2R, SWC, Date, Log, X., Time, ID, Plot, Treatment) %>% 
+  group_by(ID) %>% 
+  summarize(Ci.midpoint = (min(Ci.x)+max(Ci.x))/2) 
+mean(midpoints$Ci.midpoint) # 339.23
+median(midpoints$Ci.midpoint) # 333.2088
+
+# AML: can you get a bigger sample size by lowering the threshold (yes) or extrapolating?
+# using 333.21 Ci:
+df %>% 
+  select(HHMMSS, Photo.x, Cond, Ci.x, CO2R, SWC, Date, Log, X., Time, ID, Plot, Treatment) %>% 
+  group_by(ID, Treatment) %>% 
+  summarize(threshold = if_else(max(Ci.x)>333.21, TRUE, FALSE)) %>% 
+  group_by(Treatment) %>% 
+  summarize(keepers = sum(threshold)) %>% 
+  View()
+# AD = 8   AW = 8    ED = 12   EW = 14 ; 42 total
+
+# Andrew asks how many samples measured per species per treatment? So, of those that passed the threshold test, what is the breakdown?
+# first, add species code column
+df <- df %>% 
+  mutate(Spp = str_sub(ID, - 3, - 3)) 
+
+# tally "acceptable" curves per treatment.spp combo
+df %>%   
+  select(HHMMSS, Photo.x, Cond, Ci.x, CO2R, SWC, Date, Log, X., Time, ID, Plot, Treatment, Spp) %>% 
+  group_by(ID, Treatment, Spp) %>% 
+  summarize(threshold = if_else(max(Ci.x)>333.21, TRUE, FALSE)) %>% 
+  group_by(Treatment, Spp) %>% 
+  summarize(keepers = sum(threshold)) %>% 
+  View()
+# between 3-7 suitable curves for each Treatment.Spp combination (threshold=333.21) *note, one has all 3 pts too *high*
+
+# how variable are the results by species.treatment? Determines how big a sample size is enough.
+# I want to interpolate the Photo.x value at 333 Ci for each curve
+# linear interpolation:
+# approx(x,y,xout=5.019802)
+df_small <- df %>%   
+  select(HHMMSS, Photo.x, Cond, Ci.x, CO2R, SWC, Date, Log, X., Time, ID, Plot, Treatment, Spp)
+
+df_small %>% 
+  group_by(ID) %>% 
+  summarize(interpol = approx(Ci.x,Photo.x, xout=333.21)$y) %>% 
+  View()
+
+df_small %>% 
+  group_by(ID) %>% 
+  mutate(interpol = approx(Ci.x,Photo.x, xout=333.21)$y) %>% 
+  group_by(Treatment, Spp) %>% 
+  
+  View()
