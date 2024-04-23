9791d8e28f1be3dd8c81acd02c499273e88d2421
paigekouba <pvkouba@ucdavis.edu>
2024-04-22 18:30:42 -0700

    within- and between-plot tests for all-time data

--- CO2_Ch2.R
+++ CO2_Ch2.R
@@ -38,7 +38,6 @@ library(xts)
 #   dyRangeSelector()
 
 
-
 # 20s data is more complete
 
 CO2_6.8.22_5.24.23_20s <- read_csv("/Users/paigekouba/Documents/UC_Davis/2021_Winter/Quals/Proposal/Chapter 1/TinyFACE/CO2_control/CR3000_3.28.24/6.8.22_5.24.23_20s.csv", skip = 4, col_names = c("TIMESTAMP",	"RECORD",	"BattV_Avg",	"CO2signal",	"CO2ref",	"CO2elev",	"CO2test",	"DeltaObs",	"DeltaSet",	"LicorSig2",	"FlowMFC",	"AirTC",	"RH",	"WS_ms",	"PARuE",	"TurnCO2On",	"SpanObs",	"SpanAct",	"ZeroObs",	"ZeroAct"))
@@ -84,38 +83,21 @@ tail(CO2_20s) # shows that when I was there on Weds, the last time recorded was 
 # so I went back and used force_tz to change UTC to America/Los_Angeles
   
 # now graph
-oaks_iv %>% # 6/1/23 - 9/30/23
+oaks_iv %>% # 6/1/23 - 9/30/23; useful for showing broken pump
   select(TIMESTAMP, CO2ref, CO2elev, CO2test, DeltaObs, PARuE, FlowMFC) %>% 
   dygraph() %>% 
   dyRangeSelector()
 
 oaks_iv %>% # 6/1/23 - 9/30/23
-  select(TIMESTAMP, CO2ref, CO2elev, DeltaObs, WS_ms) %>% # "AirTC",	"RH",	"WS_ms",
+  select(TIMESTAMP, AirTC, RH, WS_ms, PARuE) %>% 
   dygraph() %>% 
   dySeries("WS_ms", axis = "y2") %>% # add second axis for wind
   dyRoller(rollPeriod = 180) %>% 
   dyRangeSelector()
 
-# check aCO2 against Walnut Grove
-WGC_CO2 <- read.csv("/Users/paigekouba/Documents/UC_Davis/2021_Winter/Quals/Proposal/Chapter 1/TinyFACE/CO2_control/WGC_CO2.csv")
-
-WGC_CO2 <- WGC_CO2 %>% 
-  mutate(timestamp = gsub("[^0-9.-:]", "", datetime)) %>% 
-  mutate(timestamp = ymd_hms(timestamp))
-
-WGC_CO2 %>% 
-  select (timestamp, value) %>% 
+oaks_iv %>% # 6/1/23 - 9/30/23
+  select(TIMESTAMP, LicorSig2, PARuE) %>% 
   dygraph() %>% 
-#  dyRoller(rollPeriod = 10) %>% 
-  dyRangeSelector()
-
-CO2_20s %>% 
-#  filter(TIMESTAMP < "2023-01-01") %>% 
-  filter(CO2elev - CO2ref > 25) %>% 
-  filter(PARuE >= 50) %>% 
-  select(TIMESTAMP, CO2ref) %>% 
-  dygraph() %>% 
-  dyRoller(rollPeriod = 180) %>% # hourly avg (of 20s data)
   dyRangeSelector()
 
 
@@ -130,12 +112,38 @@ CO2_20s %>%
 
 # all-time ∆CO2 distribution (during daytime, when CO2 tank is full and pump is on)
 CO2_operational <- CO2_20s %>% 
-  filter(DeltaObs > 25) %>% # should knock out empty-tank days and broken-pump days
+ # filter(DeltaObs > 25) %>% # should knock out empty-tank days and broken-pump days ....
   filter(PARuE >= 50) %>% # restricts to just daytime (on) values
-  filter(TIMESTAMP < "2023-11-22") # leaves out GrassFACE era
+  filter(TIMESTAMP < "2023-11-22") %>%  # leaves out GrassFACE era
+  filter(TIMESTAMP > "2023-08-01") # reliable CO2 period
+# mean of DeltaObs all time until 11/22/23 = 155 ppm
+mean(CO2_operational$DeltaObs) # 157.7379
+sd(CO2_operational$DeltaObs)# 35.0
+# what if you pretend the set point is the mean
+sum(CO2_operational$DeltaObs < 157.7*1.1 & CO2_operational$DeltaObs > 157.7*0.9)/nrow(CO2_operational) # 33.5% of the time within 10%
+sum(CO2_operational$DeltaObs < 157.7*1.2 & CO2_operational$DeltaObs > 157.7*0.8)/nrow(CO2_operational) # 59.9% of the time within 20%
+sum(CO2_operational$DeltaObs < 157.7*1.25 & CO2_operational$DeltaObs > 157.7*0.75)/nrow(CO2_operational) # 69.3% of the time within 25%
 
-mean(CO2_operational$DeltaObs) # 155.0 ppm
+sum(CO2_operational$CO2elev < (CO2_operational$CO2ref+157.7)*1.2 & CO2_operational$CO2elev > (CO2_operational$CO2ref+157.7)*0.8)/nrow(CO2_operational)
+#[1] 0.9330214 within ∆157.7
+
+
 plot(density(CO2_operational$DeltaObs))
+
+mean(CO2_operational$CO2elev) # 549
+sd(CO2_operational$CO2elev) # 84
+
+mean(CO2_operational$CO2ref) # 391
+sd(CO2_operational$CO2ref) # 61
+
+# check on all-time DeltaObs
+CO2_20s %>% 
+  filter(DeltaObs > 25) %>% # should knock out empty-tank days and broken-pump days
+  filter(PARuE >= 200) %>%
+  select(TIMESTAMP, CO2ref) %>% 
+  dygraph() %>% 
+  dyRoller(rollPeriod = 60) %>% 
+  dyRangeSelector()
 
 ggplot(CO2_operational, aes(x=DeltaObs))+
   geom_density()+
@@ -156,14 +164,21 @@ between_plot <- CO2_20s %>%
   dyRoller(rollPeriod = 10) %>% 
   dyRangeSelector()
 
+# avg_between <- between_plot %>% 
+#   group_by(TIMESTAMP = cut(TIMESTAMP, breaks = "30 min")) %>% 
+#   summarize(mDeltaTest = mean(DeltaTest), se = sd(DeltaTest)/sqrt(length(DeltaTest)))
+
 avg_between <- between_plot %>% 
   group_by(TIMESTAMP = cut(TIMESTAMP, breaks = "30 min")) %>% 
-  summarize(mDeltaTest = mean(DeltaTest), se = sd(DeltaTest)/sqrt(length(DeltaTest)))
+  summarize(mDeltaTest = mean(DeltaTest), sd = sd(DeltaTest))
 
 avg_between$Plot <- as.character(c(4:16,1:3))
+# ggplot(avg_between, aes(Plot, mDeltaTest)) +
+#   geom_point() +
+#   geom_pointrange(data = avg_between, aes(ymin=(mDeltaTest - se), ymax = (mDeltaTest + se)))
 ggplot(avg_between, aes(Plot, mDeltaTest)) +
   geom_point() +
-  geom_pointrange(data = avg_between, aes(ymin=(mDeltaTest - se), ymax = (mDeltaTest + se)))
+  geom_pointrange(data = avg_between, aes(ymin=(mDeltaTest - sd), ymax = (mDeltaTest + sd)))
 
 # within-plot testing: 3/27/24
 
@@ -177,13 +192,54 @@ within_plot <- CO2_20s %>%
   dyRoller(rollPeriod = 10) %>% 
   dyRangeSelector()
 
+# avg_within <- within_plot %>% 
+#   group_by(TIMESTAMP = cut(TIMESTAMP, breaks = "15 min")) %>% 
+#   summarize(mDeltaTest = mean(DeltaTest), se = sd(DeltaTest)/sqrt(length(DeltaTest)))
 avg_within <- within_plot %>% 
   group_by(TIMESTAMP = cut(TIMESTAMP, breaks = "15 min")) %>% 
-  summarize(mDeltaTest = mean(DeltaTest), se = sd(DeltaTest)/sqrt(length(DeltaTest)))
+  summarize(mDeltaTest = mean(DeltaTest), sd = sd(DeltaTest))
 
 avg_within$Position <- c("center", LETTERS[1:12], paste0(seq(20,100,by=10),"cm"))
 avg_within$Position <- factor(avg_within$Position, levels = unique(avg_within$Position))
 ggplot(avg_within, aes(Position, mDeltaTest)) +
   geom_point() +
-  geom_pointrange(data = avg_within, aes(ymin=(mDeltaTest - se), ymax = (mDeltaTest + se)))
+  geom_pointrange(data = avg_within, aes(ymin=(mDeltaTest - sd), ymax = (mDeltaTest + sd)))
 
+CO2_20s %>% 
+ # filter(DeltaObs > 25) %>% # should knock out empty-tank days and broken-pump days
+#  filter(PARuE >= 50) %>% # restricts to just daytime (on) values
+  filter( TIMESTAMP < "2023-11-23" &  TIMESTAMP > "2023-08-01") %>% 
+  select(TIMESTAMP, RH, LicorSig2) %>% 
+  dygraph() %>% 
+  dyRangeSelector()
+
+CO2_20s %>% # check windspeed
+    # filter(DeltaObs > 25) %>% # should knock out empty-tank days and broken-pump days
+   #  filter(PARuE >= 50) %>% # restricts to just daytime (on) values
+     filter( TIMESTAMP < "2023-11-23" &  TIMESTAMP > "2023-08-01") %>% 
+     select(TIMESTAMP, WS_ms) %>% 
+     dygraph() %>% 
+     dyRangeSelector() %>% dyRoller(rollPeriod = 180)
+
+# check aCO2 against Walnut Grove
+# https://gml.noaa.gov/dv/data/index.php?type=Surface%252BPFP&frequency=Discrete&site=WGC&parameter_name=Carbon%2BDioxide
+WGC_CO2 <- read.csv("/Users/paigekouba/Documents/UC_Davis/2021_Winter/Quals/Proposal/Chapter 1/TinyFACE/CO2_control/WGC_CO2.csv")
+
+WGC_CO2 <- WGC_CO2 %>% 
+  mutate(timestamp = gsub("[^0-9.-:]", "", datetime)) %>% 
+  mutate(timestamp = ymd_hms(timestamp))
+
+WGC_CO2 %>% 
+  select (timestamp, value) %>% 
+  dygraph() %>% 
+  #  dyRoller(rollPeriod = 10) %>% 
+  dyRangeSelector()
+
+CO2_20s %>% # graph of CO2ref
+  #  filter(TIMESTAMP < "2023-01-01") %>% 
+  filter(CO2elev - CO2ref > 25) %>% 
+  filter(PARuE >= 50) %>% 
+  select(TIMESTAMP, CO2ref) %>% 
+  dygraph() %>% 
+  dyRoller(rollPeriod = 180) %>% # hourly avg (of 20s data)
+  dyRangeSelector()
