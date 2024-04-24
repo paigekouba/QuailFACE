f7f1776327adcef8e43bf133fdd99664fc680d75
paigekouba <pvkouba@ucdavis.edu>
2024-04-24 15:52:57 -0700

    Figures for Ch 2

---
+++ Ch2_Figures.R
@@ -0,0 +1,17 @@
+# Chapter 2 Figures
+# run after 4.9.24_testing 
+
+# Daytime CO2 Elevation 4/9-4/19
+daytimeCO2_4.9to4.19
+
+# CO2 elevation between plots
+between_boxplots
+
+# Interpolated ∆CO2 for horizontal control
+interpolated_CO2
+
+# CO2 elevation above plots, with a shaded area showing height within the screen
+above_boxplots
+
+# CO2 elevation with varying vegetation height
+veg_boxplot
