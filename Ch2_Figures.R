e5f83f05e2c97693df638d695651468335951949
paigekouba <pvkouba@ucdavis.edu>
2024-06-08 12:00:59 -0700

    changed width/ht, updated final figures (pointranges instead of boxplots, add weather data)

--- Ch2_Figures.R
+++ Ch2_Figures.R
@@ -1,23 +1,26 @@
 # Chapter 2 Figures
 # run after 4.9.24_testing 
 
-pdf(file = "Ch2_figures_4.28.24.pdf")
+pdf(file = "Ch2_figures_6.6.24.pdf", width=6, height=5)
 
 # Daytime CO2 Elevation 4/9-4/19
 daytimeCO2_4.9to4.19
 
 # CO2 elevation between plots
-between_boxplots
+between_pointrange
 
 # Interpolated ∆CO2 for horizontal control
 interpolated_CO2
 
 # CO2 elevation above plots, with a shaded area showing height within the screen
-above_boxplots
+above_pointrange
 
 # CO2 elevation with varying vegetation height
-veg_boxplot
+veg_pointrange
 
 # CO2 elevation over 10d test: eCO2, aCO2 1 min averages (hourly?) and wind
+
+# Quail Ridge average daily temperature and cumulative rainfall
+quail_weather
 
 dev.off()
