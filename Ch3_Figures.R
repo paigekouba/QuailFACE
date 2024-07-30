cf7d5e746a72504f305497cdae4258d91827c7cd
paigekouba <pvkouba@ucdavis.edu>
2024-07-30 01:09:44 -0700

    updating figures for Ch 3

--- Ch3_Figures.R
+++ Ch3_Figures.R
@@ -1,8 +1,20 @@
 # Ch 3 Figures
 # Friday 7/12/24
 
-pdf(file = "Ch3_figures_7.12.24.pdf", width=6, height=5)
+#pdf(file = "Ch3_figures_7.12.24.pdf", width=6, height=5)
 # Figures
+grid.arrange(z_bootV, z_bootL, nrow=2) # mean change (in z scores) with watering for aCO2 and eCO2 conditions
+
+grid.arrange(
+  ggpredict(lm(Anet~rescale(CO2)*rescale(SWC)+time_scaled, data=plotmeans.L),
+            terms=c("CO2","SWC [4,42]"))%>% plot(rawdata=T,ci=T,colors=c("red","blue")) + labs(title="Anet (µmol CO2/m2/s), Live Oak"), 
+  ggpredict(lm(Ht.mm..8~rescale(CO2)*rescale(SWC), data=plotmeans.L), 
+            terms=c("CO2","SWC [4,42]"))%>% plot(rawdata=T,ci=T,colors=c("red","blue")) + labs(title="Final Ht (mm), Live Oak"),
+  ggpredict(lm(SRL~rescale(CO2)*rescale(SWC), data=plotmeans.L), 
+            terms=c("CO2","SWC [4,42]"))%>% plot(rawdata=T,ci=T,colors=c("red","blue")) + labs(title="Specifc Root Length (mm/g), Live Oak"),
+  ggpredict(glm(as.numeric(resprout) ~ rescale(CO2)*rescale(meanSWC), family = "binomial", data = filter(herb_list, Spp=="V")), 
+            terms=c("CO2","meanSWC [4,42]"))%>% plot(rawdata=T,ci=T,colors=c("red","blue"), jitter=0.07) + labs(title="Resprouting Rates, Valley Oak"))
+
 inv_all_nfh %>% # for seedlings without *full* herbivory, how did height change over the course of the study?
   group_by(Spp, Tmt, value) %>% 
   summarise(mean_ht = mean(ht_mm, na.rm = TRUE),se_ht = sd(ht_mm, na.rm = TRUE)/sqrt(n())) %>% 
@@ -16,23 +28,11 @@ inv_all_nfh %>% # for seedlings without *full* herbivory, how did height change 
   xlab(label = "Date") +
   facet_grid(~ Spp, labeller = as_labeller(c("L" = "Q. wislizeni", "V"= "Q. lobata"))) + theme_classic(base_size = 19)
 
-grid.arrange(pct_bootV, pct_bootL, nrow=2) # mean % change with watering for aCO2 and eCO2 conditions
-
-grid.arrange(
-  ggpredict(lm(Anet~rescale(CO2)*rescale(SWC)+time_scaled, data=plotmeans.L),
-            terms=c("CO2","SWC [4,42]"))%>% plot(rawdata=T,ci=T,colors=c("red","blue")) + labs(title="Anet (µmol CO2/m2/s), Live Oak"), 
-  ggpredict(lm(Ht.mm..8~rescale(CO2)*rescale(SWC), data=plotmeans.L), 
-            terms=c("CO2","SWC [4,42]"))%>% plot(rawdata=T,ci=T,colors=c("red","blue")) + labs(title="Final Ht (mm), Live Oak"),
-  ggpredict(lm(SRL~rescale(CO2)*rescale(SWC), data=plotmeans.L), 
-            terms=c("CO2","SWC [4,42]"))%>% plot(rawdata=T,ci=T,colors=c("red","blue")) + labs(title="Specifc Root Length (mm/g), Live Oak"),
-  ggpredict(lm(tot_area~rescale(CO2)*rescale(SWC), data=plotmeans.V), 
-            terms=c("CO2","SWC [4,42]"))%>% plot(rawdata=T,ci=T,colors=c("red","blue")) + labs(title="Total Leaf Area (mm2), Valley Oak"))
-
 # Supplement:
 quail_weather # average T and rainfall during experiment
 
-CO2_Oaks # monthly mean and sd for e and aCO2, all time
+CO2_Oaks + theme(panel.background = element_blank(), axis.line = element_line(colour = "black")) # monthly mean and sd for e and aCO2, all time
 
 plot_CO2 # per-plot mean and sd of CO2 tmt
 
-dev.off()
+#dev.off()
